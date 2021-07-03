4535c3fd1ac73dea22d246218503d14a1bbf3782
bruno.amorim <bruno.amorim@opah.com.br>
2021-07-03 12:21:07 -0300

    ajustes para o angular

--- src/main/java/com/servico/cliente/rest/ClienteController.java
+++ src/main/java/com/servico/cliente/rest/ClienteController.java
@@ -4,6 +4,7 @@ import com.servico.cliente.domain.Cliente;
 import com.servico.cliente.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/cliente")
+@CrossOrigin("http://localhost:4200")
 public class ClienteController {
 
     @Autowired
